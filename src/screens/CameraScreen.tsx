84097a985c0ec579b61f74c965e288d0000d1abc
phungdaihiep <phungdaihiep98.business@gmail.com>
2020-05-23 14:48:00 +0700

    feat: add WelcomeScreen.tsx and CameraScreen.tsx

---
+++ src/screens/CameraScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+type IProps = {};
+
+export const CameraScreen: React.FC<IProps> = (props) => {
+  return (
+    <View>
+      <Text>CameraScreen</Text>
+    </View>
+  );
+};
